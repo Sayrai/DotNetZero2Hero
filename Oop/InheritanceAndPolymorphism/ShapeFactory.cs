1c93c38277bac7819e884609447fb2e74285dddc
omoikesarah@gmail.com <repo@carfacts.ng>
2022-04-27 05:41:50 +0100

    Check the first 3 input characters and returrn the shape

--- Oop/InheritanceAndPolymorphism/ShapeFactory.cs
+++ Oop/InheritanceAndPolymorphism/ShapeFactory.cs
@@ -2,17 +2,33 @@
 {
     using System;
 
+
     public class ShapeFactory
     {
         public static Shape Create(string name)
         {
-            switch (name)
+
+            var inputFirstThreeLetters = name.Substring(0, 3);
+
+            if (inputFirstThreeLetters == "Cir")
             {
-                case nameof(Circle): return new Circle();
-                case nameof(Triangle): return new Triangle();
-                case nameof(Rectangle): return new Rectangle();
-                default: throw new Exception("Invalid shape");
+                return new Circle();
+           
+            } else if(inputFirstThreeLetters == "Tri")
+            {
+                return new Triangle();
+
+            } else if(inputFirstThreeLetters == "Rec")
+
+            {
+                return new Rectangle();
             }
+
+            else
+            {
+                throw new Exception("Invalid shape, first 3 letters doesn't start with any valid letters");
+            }
+           
         }
     }
 }
